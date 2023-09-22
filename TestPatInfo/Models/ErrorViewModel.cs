f815fde24c0629c8a90a5f5d728962ae508f36dd
rayli <rayrhan312@gmail.com>
2023-09-22 18:38:30 +0800

    建立專案

---
+++ TestPatInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestPatInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
